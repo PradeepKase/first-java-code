3b0d53a4077825de07a5bbda7697670321ffe50d
PradeepKase <pkase6@gmail.com>
2022-09-04 20:42:44 +0530

    first code in java

---
+++ sampletest/Sampletest.java
@@ -0,0 +1,12 @@
+package sampletest;
+
+public class Sampletest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0; i<5; i++) {
+			System.out.println("Pradeep Kase");
+		}
+	}
+
+}
